50d594a87170a0837eff7bccd05feb611af62e10
ildarianii <ildariani141@gmail.com>
2025-09-09 11:43:04 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,10 @@
+package pkg100dayscoding;
+public class Days2CodingISC {
+    public static void main(String[] args) {
+      int a = 18;
+        System.out.print("Nama saya");
+        System.out.println("Ilda riani");
+        System.out.printf("Umur saya %d tahun", a);
+        
+            }
+}
