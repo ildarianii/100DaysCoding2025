fc2ccf51e4d210487e3261b46d8c2a018e094833
ildarianii <ildariani141@gmail.com>
2025-09-10 16:34:44 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,10 @@
+package pkg100dayscodingday3;
+public class days3Coding {
+    public static void main(String[] args) {
+   // komentar dengan satu baris
+   
+   /* ini digunakan untuk komentar 
+      lebih dari satu baris */
+   
+    }
+}
