9fae8847261f3ab2f7d8a8a3eafc2fddfb416b3b
ildarianii <ildariani141@gmail.com>
2025-09-11 22:37:25 +0800

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,21 @@
+package pkg100dayscodingday3;
+public class day4Coding {
+public static void main(String[] args) {
+        // 1. \n = penggunnaan baris baru
+        System.out.println("Haii perkenalkan\n nama saya ilda");
+        // 2. \t = digunakan untuk tab
+        System.out.println("Nama saya: \t ilda riani");
+        // 3. \\ = untuk penggunaan backslash (\)
+        System.out.println("ilda\\riani");
+        // 4. \' = menampilkan tanda kutip tunggal(')
+        System.out.println("It\'s ilda");
+        // 5. \" = menampilkan tanda kutip ganda
+        System.out.println("\"ilda riani\"");
+        // 6. \r = mengembalikan ke awal baris
+        System.out.println("ilda\r riani");
+        // 7. \b = menghilangkan karakter terakhir
+        System.out.println("ildaa\b riani");
+        // 8. \f = digunakan pada saat memajukan halaman
+        System.out.println("ilda \f riani");
+    }
+}
