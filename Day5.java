03de705d5314ec657fe0a511adc1d8be108959ea
ildarianii <ildariani141@gmail.com>
2025-09-12 00:21:53 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+package pkg100dayscodingday5;
+public class day5 {
+    public static void main(String[] args) {
+    byte a = 120;
+    short b = 1200;
+    int c = 12000;
+    long d = 120000;
+    
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+  
+    }
+}
