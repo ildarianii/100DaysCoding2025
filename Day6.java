6f69d9ae2401d502422ee7533b69fe35e032e328
ildarianii <ildariani141@gmail.com>
2025-09-13 09:54:44 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+package pkg100dayscodingday6;
+public class day6 {
+    public static void main(String[] args) {
+    double u = 123.0;
+    float v = 130.0f;
+    
+        System.out.println(u);
+        System.out.println(v);
+    }
+}
