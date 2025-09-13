da345b28feba33249cdafae2b317e8c081725440
ildarianii <ildariani141@gmail.com>
2025-09-14 00:11:26 +0800

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,11 @@
+package pkg100dayscoodingday7;
+public class day7 {
+    public static void main(String[] args) {
+        char j = 'w';
+        boolean o = true;
+        
+        System.out.println(j);
+        System.out.println(o);
+    }
+    
+}
