02771fd34606c9fde8c7142304c63d1a5e2a1393
ildarianii <ildariani141@gmail.com>
2025-09-16 19:43:40 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,13 @@
+package pkg100daycodingisc;
+public class Day9 {
+    public static void main(String[] args) {
+   // deklarasi
+    int i;
+   // inisialisasi/ memberikan nilai 
+    i = 14;
+    System.out.println("angka awal: " + i);
+   // update variabel
+    i = 18;
+    System.out.println("angka setelah update: " + i);
+    } 
+}
