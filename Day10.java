2b5ff4ec18448a47db53d9af78d70cd6dcd8cc37
ildarianii <ildariani141@gmail.com>
2025-09-17 21:49:50 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,8 @@
+package pkg100daycodingisc;
+public class Day10 {
+    public static void main(String[] args) {
+       final String name = "ilda rianii";
+        System.out.println(name);
+    // bersifat FINAL (tidak dapat di update atau diubah)    
+    }
+}
