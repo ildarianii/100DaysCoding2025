1425521002199ea1235ed27f6ed01ddd0965c408
ildarianii <ildariani141@gmail.com>
2025-09-22 00:34:30 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+package pkg100daycodingisc;
+public class Day15 {
+    public static void main(String[] args) {
+      int angka1 = 22;  
+      int angka2 = 24; 
+      
+      int pj = angka1 + angka2;
+      int pg = angka2 - angka1;
+      int d = angka2 - angka1 + angka2;
+      
+      System.out.println("Penjumlahan: " + pj);
+      System.out.println("Pengurangan: " + pg);
+      System.out.println("Campuran: " + d);
+    }    
+}
