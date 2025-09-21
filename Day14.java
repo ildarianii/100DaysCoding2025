950fd44b8d0d3854837e76cd60d1af47c2e11e54
ildarianii <ildariani141@gmail.com>
2025-09-21 22:29:22 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,17 @@
+package pkg100daycodingisc;
+public class Day14 {
+    public static void main(String[] args) {
+        String TinggiB = "15.8";
+        String Nowa = "087777777512";
+        String Umurr = "18";
+        
+        double Tinggii = Double.parseDouble(TinggiB);
+        long Nowaa = Long.parseLong(Nowa);
+        int Umuur = Integer.parseInt(Umurr);
+        
+        System.out.println("Tinggi: " + Tinggii);
+        System.out.println("No wa: " + Nowaa);
+        System.out.println("Umur: " + Umurr);
+          
+    }
+}
