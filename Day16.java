73da175c2940fc75bc627ed2aa5a5e405cdcf551
ildarianii <ildariani141@gmail.com>
2025-09-23 13:23:40 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,13 @@
+package pkg100daycodingisc;
+public class Day16 {
+public static void main(String[] agrs ){
+    int ilda = 14;
+    int riani = 2;
+    
+    int ab = ilda * riani;
+    int ac = ilda / riani;
+    
+    System.out.println(" Hasil Perkalian: " + ab);
+    System.out.println(" Hasil Pembagian: " + ac);
+}
+}
