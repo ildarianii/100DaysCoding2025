22d874b3f810f7e8115694dc81545efdb12d2aa7
ildarianii <ildariani141@gmail.com>
2025-09-24 21:15:42 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,11 @@
+package pkg100daycodingisc;
+public class Dday17 {
+    public static void main(String[] args) {
+        int angka = 14;
+        int angka2 = 13;
+        
+        int a = angka % angka2;
+        
+        System.out.println("Hasil modulus: " + a);
+    }
+}
