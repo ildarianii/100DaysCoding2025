b04fbbe6ed244bf024260654985294572d5b664f
ildarianii <ildariani141@gmail.com>
2025-09-25 22:06:48 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,15 @@
+package pkg100daycodingisc;
+public class Day18 {
+    public static void main(String[] args) {
+        byte angka = 14;
+        short angka2 = angka;
+        int angka3 = angka2;
+        long angka4 = angka3;
+        
+        System.out.println("byte: " + angka);
+        System.out.println("short: " + angka2);
+        System.out.println("int: " + angka3);
+        System.out.println("angka4: " + angka4);
+        
+    }  
+}
