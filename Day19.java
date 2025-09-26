a37c4a8b81f374a87c059d836b2f82498ac39694
ildarianii <ildariani141@gmail.com>
2025-09-26 22:56:31 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,8 @@
+package pkg100daycodingisc;
+public class Day19 {
+    public static void main(String[] args) {
+    double angka = 14.1;
+    int angka2 = (int) angka;
+        System.out.println(angka2);
+    }
+}
