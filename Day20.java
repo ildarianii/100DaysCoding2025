906086a34fdc38adf9fc5c34f658178e8a9a283a
ildarianii <ildariani141@gmail.com>
2025-09-27 21:26:31 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,25 @@
+package pkg100daycodingisc;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+        // cara pertama
+        char inisial = 'I';
+        String st = String.valueOf(inisial);
+
+        System.out.println(inisial);
+
+        // caraa kedua
+        int tl = 14;
+        String teks = tl + "";
+
+        System.out.println(teks);
+
+        // cara ketiga
+        double db = 14.1;
+        String str = Double.toString(db);
+
+        System.out.println(str);
+
+    }
+}
