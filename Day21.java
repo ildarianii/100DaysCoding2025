c4166f1e665e7690872e3769f4406e40708d9288
ildarianii <ildariani141@gmail.com>
2025-09-28 21:54:25 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,15 @@
+package pkg100daycodingisc;
+public class Day21 {
+    public static void main(String[] args) {
+        int angka1 = 14;
+        int angka2 = 9;
+        
+        angka1 = angka1 + angka2;
+        angka2 = angka1 - angka2;
+        angka1 = angka1 - angka2;
+        
+        System.out.println("angka 1 = " + angka1);
+        System.out.println("angka 2 = " + angka2);
+    }
+    
+}
