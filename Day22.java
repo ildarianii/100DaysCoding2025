241cc3b4d6c812d157d6b3df778151e9f6c2feb2
ildarianii <ildariani141@gmail.com>
2025-09-29 21:16:09 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,11 @@
+package pkg100daycodingisc;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+       int sisi = 14;
+       int luas = sisi * sisi;
+       
+        System.out.println("Luas persegi = " + luas);   
+    }
+}
