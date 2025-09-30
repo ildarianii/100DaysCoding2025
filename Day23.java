5415ca3945739c242a38f45e318af81a5c0814a5
ildarianii <ildariani141@gmail.com>
2025-09-30 21:44:01 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,15 @@
+
+public class Day23 {
+
+    public static void main(String[] args) {
+        int panjang = 20;
+        int lebar = 14;
+        
+        int luas = panjang * lebar;
+        
+        System.out.println("Luas persegi panjang = " + luas);
+        
+    }
+
+}
+
