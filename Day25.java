97afa6342d399f988e37af2246cfc5649f9a4800
ildarianii <ildariani141@gmail.com>
2025-10-02 23:11:40 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,14 @@
+package pkg100daycodingisc;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        int a = 14;
+        a++;
+        System.out.println(a);
+        
+        int b = 12;
+        b--;
+        System.out.println(b); 
+    }
+}
