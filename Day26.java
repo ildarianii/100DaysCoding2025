55fbcbf5e0f4be02e08f1dbf522824fe03ef6cee
ildarianii <ildariani141@gmail.com>
2025-10-03 22:20:52 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,20 @@
+public class Day26 {
+
+    public static void main(String[] args) {
+        int angka = 14;
+        System.out.println("Nilai awal = " + angka);
+        
+        angka += 4;
+        System.out.println("Penjumlahan = " + angka);
+        angka -= 4;
+        System.out.println("Pengurangan = " + angka);
+        angka *= 4;
+        System.out.println("Perkalian = " + angka);
+        angka /= 4;
+        System.out.println("Pembagian = " + angka);
+        angka %= 4;
+        System.out.println("Modulus = " + angka);
+                
+    }
+
+}
