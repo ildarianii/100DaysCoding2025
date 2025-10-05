5948b703ca5e1deb9c60717dc57806b6265836ea
ildarianii <ildariani141@gmail.com>
2025-10-05 21:40:07 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day28 {
+    public static void main(String[] args) {
+        int angka1 = 14;
+        int angka2 = 9;
+        
+        System.out.println(angka1 == angka2);
+        System.out.println(angka1 != angka2);
+    }
+}
