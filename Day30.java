e8ee8b661ad3b1390b78e7b55b9561b9a144bb1b
ildarianii <ildariani141@gmail.com>
2025-10-07 21:41:08 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,11 @@
+package pkg100daycodingisc;
+public class Day30 {
+    public static void main(String[] args) {
+        int angka1 = 14;
+        int angka2 = 9;
+        
+        System.out.println(angka1 <= angka2);
+        System.out.println(angka1 >= angka2);
+    }
+    
+}
