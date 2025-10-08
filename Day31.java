579cfc11ce04ce6caed91af6c35ef4f11858003b
ildarianii <ildariani141@gmail.com>
2025-10-08 22:18:53 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,12 @@
+package pkg100daycodingisc;
+public class Day31 {
+    public static void main(String[] args) {
+        int nilai1 = 14;
+        int nilai2 = 9;
+        
+        System.out.println(nilai1 == nilai2 && nilai2 == nilai1);
+        System.out.println(nilai1 > nilai2 && nilai2 < nilai1);
+        
+    }
+    
+}
