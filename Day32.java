915302acfdb4cc3fd365e40e5a38c82aa0cb1918
ildarianii <ildariani141@gmail.com>
2025-10-09 23:16:55 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day32 {
+    public static void main(String[] args) {
+        int angka1 = 14;
+        int angka2 = 9;
+        
+        System.out.println(angka1 == angka2 || angka2 == angka1 );
+        System.out.println(angka1 > angka2 || angka2 > angka1 );
+    }
+}
