c6ffa88649a3ca42e367026c5aa3a3fe855b3236
ildarianii <ildariani141@gmail.com>
2025-10-10 23:22:04 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day33 {
+    public static void main(String[] args) {
+        boolean a = true;
+        
+        System.out.println("nilai awal: "+ a);
+        System.out.println("nilai akhir: "+ !a);
+    }
+    
+}
