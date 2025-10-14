fb2aa23d14ee1469a52472da1018de9af2fd58e7
ildarianii <ildariani141@gmail.com>
2025-10-14 21:12:05 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,19 @@
+package pkg100daycodingisc;
+
+import java.util.Scanner;
+
+public class Day37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = sc.nextInt();
+        
+        if (angka % 2 == 1) {
+            System.out.println("Angka ganjil");
+            
+        }else{
+            System.out.println("Angka genap");
+        }
+    }
+    
+}
