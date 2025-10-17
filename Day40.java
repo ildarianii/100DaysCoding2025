5b32743f05e351ce47e7a5580c270b183ae97c6b
ildarianii <ildariani141@gmail.com>
2025-10-17 22:42:55 +0800

    Day40.java

---
+++ Day40.java
@@ -0,0 +1,43 @@
+package pkg100daycodingisc;
+
+import java.util.Scanner;
+
+public class Day40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = sc.nextDouble();
+        
+        System.out.print("Masukkan operator (+,-,*,/): ");
+        char op = sc.next().charAt(0);
+        
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = sc.nextDouble();
+        
+        double hasil;
+        
+        if (op == '+') {
+            hasil = angka1 + angka2;
+            System.out.println("Hasil = "+ hasil);
+            
+        }else if (op == '-') {
+            hasil = angka1 - angka2;
+            System.out.println("Hasil = "+ hasil); 
+            
+        }else if (op == '*') {
+            hasil = angka1 * angka2;
+            System.out.println("Hasil = "+ hasil); 
+            
+        }else if (op == '/') {
+            hasil = angka1 / angka2;
+            System.out.println("Hasil = "+ hasil); 
+            
+        }else if (op == '%') {
+            hasil = angka1 % angka2;
+            System.out.println("Hasil = "+ hasil); 
+            
+        }
+          
+    }
+}
+  
