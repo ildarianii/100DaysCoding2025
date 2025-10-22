037eb22ea7c7c7e5458ca28ef9ee87b026d38744
ildarianii <ildariani141@gmail.com>
2025-10-22 21:07:38 +0800

    Day45.java

--- Day45.java
+++ Day45.java
@@ -6,9 +6,9 @@ public class Day45 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan kode hari (1,2,3): ");
-        int kode = sc.nextInt();
+        int hari = sc.nextInt();
         
-        switch (kode){
+        switch (hari){
             case 1 :
                 System.out.println("Senin");
             break;
@@ -17,6 +17,10 @@ public class Day45 {
             break;
             case 3 :
                 System.out.println("Rabu");
+            break;
+            
+            default :
+                System.out.println("KODE HARI TIDAK DITEMUKAN");
             break;
         }
     }
