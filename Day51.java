141e7769b84fee9b432efc29f9d5acd027af00c7
ildarianii <ildariani141@gmail.com>
2025-10-28 22:37:51 +0800

    Day51.java

---
+++ Day51.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day51 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("angka ke-" +i);
+            
+        }
+    }
+    
+}
