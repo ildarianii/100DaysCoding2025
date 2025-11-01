9fb1795e4905d2a59f5621c6a562e340e0cae715
ildarianii <ildariani141@gmail.com>
2025-11-01 22:26:30 +0800

    Day55.java

---
+++ Day55.java
@@ -0,0 +1,18 @@
+package pkg100daycodingisc;
+
+import java.util.Scanner;
+
+public class Daay55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nilai: ");
+        int angka = sc.nextInt();
+        
+        if (angka > 70) {
+            System.out.println("Ujian Kroco Ji Pale");
+            
+        }else{
+            System.out.println("Sa Jappo'ka Aih");
+        } 
+    }  
+}
