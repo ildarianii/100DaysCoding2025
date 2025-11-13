2c962e4a936548f346e5d2350bb65a57cf76121c
ildarianii <ildariani141@gmail.com>
2025-11-13 21:59:28 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,17 @@
+package pkg100daycodingisc;
+
+import java.util.Scanner;
+
+public class Day67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int nilai = sc.nextInt();
+        
+        for (int i = 1; i <= nilai; i++) {
+            System.out.print("* ");
+            
+        }
+    }
+    
+}
