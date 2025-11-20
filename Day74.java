c8afb4d9e6600539176a7f546da22cc5e8267f04
ildarianii <ildariani141@gmail.com>
2025-11-20 22:33:10 +0800

    Day74.java

--- Day74.java
+++ Day74.java
@@ -6,16 +6,16 @@ public class Day74 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
         int pilihan;
-        do {
-           System.out.println("=== MENU TOKO HELLO WORLD ===");
-           System.out.println("1. Roti -- Rp5.000");
-           System.out.println("2. Matcha -- Rp25.000");
-           System.out.println("3. Kopi -- Rp15.000");
-           System.out.println("4. Teh -- Rp12.000");
+        System.out.println("=== MENU TOKO HELLO WORLD ===");
+        System.out.println("1. Roti -- Rp5.000");
+        System.out.println("2. Matcha -- Rp25.000");
+        System.out.println("3. Kopi -- Rp15.000");
+        System.out.println("4. Teh -- Rp12.000");
            
+        do {
             System.out.print("\nMasukkan pilihan anda disini^^: ");
             pilihan = sc.nextInt();
-            int harga = 0;
+           int harga = 0;
 
             switch (pilihan){
                 case 1 :
@@ -23,7 +23,7 @@ public class Day74 {
                     System.out.println("Anda membeli Roti seharga Rp" + harga);
                     System.out.println("Terimakasih sudah berbelanja:)");
                     break;
-
+                    
                 case 2 :
                     harga = 25000;
                     System.out.println("Anda membeli Matcha seharga Rp" + harga);
@@ -41,7 +41,7 @@ public class Day74 {
                     System.out.println("Anda membeli Teh seharga Rp" + harga);
                     System.out.println("Terimakasih sudah berbelanja:)");
                     break;
-
+                    
                 case 5 :
                     System.out.println("Keluar dari program..");
                     break;
