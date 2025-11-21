656a540e2c8d6a12cdae72144a52bad5b701f0ee
ildarianii <ildariani141@gmail.com>
2025-11-21 21:23:52 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,12 @@
+package pkg100daycodingisc;
+
+public class Day75 {
+
+    public static void main(String[] args) {
+        String a = "Siapapun bisa jadi apapun";
+
+        System.out.println("Length : " + a.length());
+        System.out.println("toUpperCase : " + a.toUpperCase());
+        System.out.println("toLowerCase : " + a.toLowerCase());
+    }
+}
