de7eb2b038dd7c08eac351e3b57a159ea7459dd3
ildarianii <ildariani141@gmail.com>
2025-11-26 20:47:54 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day80 {
+    public static void main(String[] args) {
+        int nilai [] = {10, 20, 30, 40, 50, 60};
+        
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.printf("Elemen ke-%d = %d\n", (i + 1), nilai[i]); 
+        } 
+    } 
+}
