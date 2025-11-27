4f74f4499fca31ba74bcb4dc6d7a0c426ac44508
ildarianii <ildariani141@gmail.com>
2025-11-27 21:32:28 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day81 {
+    public static void main(String[] args) {
+        int nilai [] = {10, 20, 30, 40, 50, 60};
+        
+        System.out.println(nilai[1]);
+        System.out.println(nilai[3]);
+    }
+    
+}
