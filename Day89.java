be8905951e440b3afb996f643f9620479a12f5d0
ildarianii <ildariani141@gmail.com>
2025-12-05 22:34:16 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,9 @@
+package pkg100daycodingisc;
+public class Day89 {
+   static void Ilda() {
+        System.out.println("Halo, kamu suka matcha kan?");
+    }
+    public static void main(String[] args) {
+        Ilda();
+    }
+}
