fcaddb564d36db94ef8dfd0879d9e21d0b335055
ildarianii <ildariani141@gmail.com>
2025-12-09 21:59:00 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,13 @@
+package pkg100daycodingisc;
+public class Day93 {
+    public static void main(String[] args) {
+        double angka1 = 14.0108;
+        double angka2 = 55.4;
+        double angka3 = 10.0920;
+        
+        System.out.println(Math.ceil(angka1));
+        System.out.println(Math.floor(angka2));
+        System.out.println(Math.round(angka3));
+    }
+    
+}
