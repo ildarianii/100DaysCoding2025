c9545a4d88ce9b31211512feea2cbd8c8de58b4b
ildarianii <ildariani141@gmail.com>
2025-12-10 22:44:02 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package pkg100daycodingisc;
+public class Day94 {
+    public static void Ilda() {
+        System.out.println("Engene");
+    }
+    public static void main(String[] args) {
+        Ilda();
+    }
+    
+}
